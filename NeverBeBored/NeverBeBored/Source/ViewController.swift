3a22cf54dc3c53201097e61323fafa5ebd4f0868
souzagabriela <ellabaptist@outlook.com>
2022-10-11 09:33:18 -0300

    Initial commit: install swiftlint and setup folders

---
+++ NeverBeBored/NeverBeBored/Source/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NeverBeBored
+//
+//  Created by Gabriela Souza Batista on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
