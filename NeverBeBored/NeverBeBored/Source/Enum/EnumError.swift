57b2ab54fe696b1f921aca6988faced9848d5bde
souzagabriela <ellabaptist@outlook.com>
2022-10-11 12:04:24 -0300

    add enum and api model

---
+++ NeverBeBored/NeverBeBored/Source/Enum/EnumError.swift
@@ -0,0 +1,14 @@
+//
+//  EnumError.swift
+//  NeverBeBored
+//
+//  Created by Gabriela Souza Batista on 11/10/22.
+//
+
+import Foundation
+
+enum EnumError: Error {
+    case invalidURL
+    case requestError
+    case invalidData
+}
---
+++ NeverBeBored/NeverBeBored/Source/Model/API Model.swift
@@ -0,0 +1,17 @@
+//
+//  API Model.swift
+//  NeverBeBored
+//
+//  Created by Gabriela Souza Batista on 11/10/22.
+//
+
+import Foundation
+
+struct BoredInfo: Codable {
+    let activity: String
+    let type: String
+    let participants: Int
+    let price: Float
+    let link: String
+    let accessibility: Float
+}
