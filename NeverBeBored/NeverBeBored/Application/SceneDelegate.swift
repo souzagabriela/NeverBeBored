0cd9c8b08569a2a045c70c830a08e2fd32063422
souzagabriela <ellabaptist@outlook.com>
2022-10-24 09:43:34 -0300

    fix bug

--- NeverBeBored/NeverBeBored/Application/SceneDelegate.swift
+++ NeverBeBored/NeverBeBored/Application/SceneDelegate.swift
@@ -18,7 +18,7 @@ func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options conn
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
-    window.rootViewController = UINavigationController(rootViewController: MainScreenController())
+    window.rootViewController = UINavigationController(rootViewController: OnboradingViewController())
         self.window = window
     }
 
